bfa8126583061e783fcdb25df8e291742968406d
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-03-31 14:37:01 +0300

    echo message about participants_with_block_signature.txt

--- scripts/unix/pick-winner.sh
+++ scripts/unix/pick-winner.sh
@@ -5,6 +5,7 @@ set -e
 ./retweets-parser $1 > participants_with_block_signature.txt
 PARTICIPANTS_TOTAL=$(jq length participants_with_block_signature.txt)
 curl -sS -X GET --header 'Accept: application/json' "https://nodes.wavesnodes.com/blocks/headers/at/$2" | jq '.["signature"]' >> participants_with_block_signature.txt
+echo "Retweeters and block signature was saved to 'participants_with_block_signature.txt'"
 PROOFS=$(./vrf-proof participants_with_block_signature.txt "$3" "$PARTICIPANTS_TOTAL")
 echo "$PROOFS"
 WINNER_INDEX=$(echo "$PROOFS" | tail -n 1 | awk 'BEGIN {FS=": ";} { print $2 }')
