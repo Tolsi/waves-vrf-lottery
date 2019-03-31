b127f6e517bff189b6be8dc0aefc6ec508c2eb6d
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-03-31 13:53:28 +0300

    print keys filenames

--- scripts/unix/create-keys.sh
+++ scripts/unix/create-keys.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+set -e
+
 openssl ecparam -name prime256v1 -genkey -out p256-key.pem -noout
+echo "Private key 'p256-key.pem' was created"
 openssl ec -in p256-key.pem -pubout -out p256-pubkey.pem
+echo "Public key 'p256-pubkey.pem' was created"
