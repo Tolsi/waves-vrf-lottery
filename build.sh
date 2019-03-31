c3c0fdc3983f844640cb67fb0f84f2e9cab6b504
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-03-31 12:59:47 +0300

    rename vrf create to proof

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-create vrf-create.go
+env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-proof vrf-proof.go
 env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-verify vrf-verify.go
 env GOOS=darwin GOARCH=386 go build -o build/mac/retweets-parser retweets-parser.go
 
-env GOOS=linux GOARCH=386 go build -o build/linux/vrf-create vrf-create.go
+env GOOS=linux GOARCH=386 go build -o build/linux/vrf-proof vrf-proof.go
 env GOOS=linux GOARCH=386 go build -o build/linux/vrf-verify vrf-verify.go
 env GOOS=linux GOARCH=386 go build -o build/linux/retweets-parser retweets-parser.go
 
-env GOOS=windows GOARCH=386 go build -o build/windows/vrf-create.exe vrf-create.go
+env GOOS=windows GOARCH=386 go build -o build/windows/vrf-proof.exe vrf-proof.go
 env GOOS=windows GOARCH=386 go build -o build/windows/vrf-verify.exe vrf-verify.go
 env GOOS=windows GOARCH=386 go build -o build/windows/retweets-parser.exe retweets-parser.go
 
--- vrf-proof.go
+++ vrf-proof.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 )
 
+// you can create the keys using:
 // openssl ecparam -name prime256v1 -genkey -out p256-key.pem -noout
 // openssl ec -in p256-key.pem -pubout -out p256-pubkey.pem
 func main() {
