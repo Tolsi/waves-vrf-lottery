e276a7e421047be3a1ca100f188b36b965543095
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-04-02 23:53:09 +0300

    pick and verify winner tools were added

--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
 rm -rf build/
 
-env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-prove vrf-prove.go
-env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-verify vrf-verify.go
-env GOOS=darwin GOARCH=386 go build -o build/mac/retweets-parser retweets-parser.go
+env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-prove pick-winner.go
+env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-verify verify-winner.go
 env GOOS=darwin GOARCH=386 go build -o build/mac/create-keys create-keys.go
 
-env GOOS=linux GOARCH=386 go build -o build/linux/vrf-prove vrf-prove.go
-env GOOS=linux GOARCH=386 go build -o build/linux/vrf-verify vrf-verify.go
-env GOOS=linux GOARCH=386 go build -o build/linux/retweets-parser retweets-parser.go
+env GOOS=linux GOARCH=386 go build -o build/linux/vrf-prove pick-winner.go
+env GOOS=linux GOARCH=386 go build -o build/linux/vrf-verify verify-winner.go
 env GOOS=linux GOARCH=386 go build -o build/linux/create-keys create-keys.go
 
-env GOOS=windows GOARCH=386 go build -o build/windows/vrf-prove.exe vrf-prove.go
-env GOOS=windows GOARCH=386 go build -o build/windows/vrf-verify.exe vrf-verify.go
-env GOOS=windows GOARCH=386 go build -o build/windows/retweets-parser.exe retweets-parser.go
+env GOOS=windows GOARCH=386 go build -o build/windows/vrf-prove pick-winner.go
+env GOOS=windows GOARCH=386 go build -o build/windows/vrf-verify verify-winner.go
 env GOOS=windows GOARCH=386 go build -o build/windows/create-keys create-keys.go
 
-zip -j build/release-mac-$(git describe --tags).zip build/mac/* scripts/unix/* scripts/mac/*
-zip -j build/release-linux-$(git describe --tags).zip build/linux/* scripts/unix/* scripts/linux/*
+zip -j build/release-mac-$(git describe --tags).zip build/mac/*
+zip -j build/release-linux-$(git describe --tags).zip build/linux/*
 zip -j build/release-windows-$(git describe --tags).zip build/windows/*
---
+++ verify-winner.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Tolsi/vrf-lottery/tools"
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/coniks-sys/coniks-go/crypto/vrf"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//region Read params
+
+	participantsAndBlockSignature, err := ioutil.ReadFile(os.Args[1])
+	s := strings.Split(string(participantsAndBlockSignature), "\n")
+	participants := s[0]
+	blockSignature := s[1]
+	tools.PrintErrorAndExit(err)
+
+	blockHeight, err := strconv.ParseInt(os.Args[2], 10, 64)
+	tools.PrintErrorAndExit(err)
+
+	vrfString := os.Args[3]
+	vrfBytes := base58.Decode(vrfString)
+
+	proofString := os.Args[4]
+	proofBytes := base58.Decode(proofString)
+
+	pkb := vrf.PublicKey(base58.Decode(os.Args[5]))
+	tools.PrintErrorAndExit(err)
+
+	//endregion
+
+	//region Create proofs
+
+	loadedBlockSignature, err := tools.GetBlockSignature(uint(blockHeight))
+	tools.PrintErrorAndExit(err)
+
+	if loadedBlockSignature != blockSignature {
+		fmt.Printf("Provable file contains different block signature at height %d: expected '%s', got '%s'\n", blockHeight, loadedBlockSignature, blockSignature)
+		os.Exit(1)
+	}
+
+	verifyResult := pkb.Verify(participantsAndBlockSignature, vrfBytes, proofBytes)
+	if !verifyResult {
+		fmt.Printf("Proof verification was failed")
+	}
+
+	//endregion
+
+	//region Result output
+
+	vrfNumber := new(big.Int)
+	vrfNumber.SetBytes(vrfBytes[:])
+
+	moduloBigint := big.NewInt(int64(len(participants)))
+	moduloResult := new(big.Int).Mod(vrfNumber, moduloBigint)
+
+	winner := participants[moduloResult.Int64()]
+
+	fmt.Printf("message: %s\n", string(participantsAndBlockSignature))
+	fmt.Printf("proof (base58): %s\n", proofString)
+	fmt.Printf("vrf bytes (base58): %s\n", base58.Encode(vrfBytes))
+	fmt.Printf("vrf as number: %d\n", vrfNumber)
+	fmt.Printf("modulo: %d\n", moduloResult)
+	fmt.Printf("winner is participant #%d: %s\n", moduloResult, winner)
+
+	//endregion
+}
