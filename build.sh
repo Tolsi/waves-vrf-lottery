86649fca902e1b1745480217f6d2bf10cadc888f
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-04-02 23:55:21 +0300

    names fix

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 rm -rf build/
 
-env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-prove pick-winner.go
-env GOOS=darwin GOARCH=386 go build -o build/mac/vrf-verify verify-winner.go
+env GOOS=darwin GOARCH=386 go build -o build/mac/pick-winner pick-winner.go
+env GOOS=darwin GOARCH=386 go build -o build/mac/verify-winner verify-winner.go
 env GOOS=darwin GOARCH=386 go build -o build/mac/create-keys create-keys.go
 
-env GOOS=linux GOARCH=386 go build -o build/linux/vrf-prove pick-winner.go
-env GOOS=linux GOARCH=386 go build -o build/linux/vrf-verify verify-winner.go
+env GOOS=linux GOARCH=386 go build -o build/linux/pick-winner pick-winner.go
+env GOOS=linux GOARCH=386 go build -o build/linux/verify-winner verify-winner.go
 env GOOS=linux GOARCH=386 go build -o build/linux/create-keys create-keys.go
 
-env GOOS=windows GOARCH=386 go build -o build/windows/vrf-prove pick-winner.go
-env GOOS=windows GOARCH=386 go build -o build/windows/vrf-verify verify-winner.go
+env GOOS=windows GOARCH=386 go build -o build/windows/pick-winner pick-winner.go
+env GOOS=windows GOARCH=386 go build -o build/windows/verify-winner verify-winner.go
 env GOOS=windows GOARCH=386 go build -o build/windows/create-keys create-keys.go
 
 zip -j build/release-mac-$(git describe --tags).zip build/mac/*
