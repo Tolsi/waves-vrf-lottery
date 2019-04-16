388a2119f6bb403ea587a56b23d1d64f223af052
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-04-16 21:44:40 +0300

    remove vrf folder

--- vrf/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-
-buildGoPackage {
-  name = "curve25519-go";
-  src = ./.;
-  goPackagePath = "github.com/Lucus16/curve25519-go";
-}
--- vrf/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Lucus16/curve25519-go
-
-go 1.12
